be7276a52e48bde772d5623f98e5e7f6610871e0
scottmr007 <mrscott@tutamail.com>
2023-12-09 16:59:34 -0500

    Contact_us page

---
+++ pages/Contact_us.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from send_email import send_email
+import pandas
+
+df = pandas.read_csv("topics.csv")
+
+with st.form(key="email_form"):
+    user_email = st.text_input("Your email address")
+    option = st.selectbox("What topic do you want to discuss?", df['topic'])
+    raw_message = st.text_area("Text")
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+Topic {option}
+{raw_message}
+"""
+
+    button = st.form_submit("Submit")
+    if button:
+        send_email(message)
+        st.info("Your email was successfully sent.")
